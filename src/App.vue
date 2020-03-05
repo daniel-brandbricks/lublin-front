05f120bf03c9323a6e57f12f222af6190d4cde6f
devDeerHead <dev@deerhead.net>
2020-03-05 18:00:14 +0100

    fxs

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,10 @@
 
   export default {
     name: 'App',
-    components: { ConfirmDeleteModal }
+    components: { ConfirmDeleteModal },
+    created () {
+      this.$store.dispatch('getCurrentUser')
+    }
   }
 </script>
 
